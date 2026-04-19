33bb0cf34af5234c4cfe31d70b87dae8a818d149
Wassim Abdine <nonowi@Wassims-iMac.fritz.box>
2026-04-19 21:25:36 +0200

    final import path cleanup

--- src/repositories/member.repository.js
+++ src/repositories/member.repository.js
@@ -1,4 +1,4 @@
-import { pool } from '../db/pool.js';
+import { pool } from '../config/pool.js';
 
 export async function findMemberByShopifyId(shopifyCustomerId) {
   const result = await pool.query(
